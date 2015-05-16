bfa300aec383ce0c15a0372e1b867d68f6163ce9
kasahi <kasahinet@gmail.com>
2015-05-16 22:08:26 +0900

    add layout settings

--- mobile/src/main/java/jp/obcn/memoleep/Activity/SettingsActivity.java
+++ mobile/src/main/java/jp/obcn/memoleep/Activity/SettingsActivity.java
@@ -1,9 +1,17 @@
 package jp.obcn.memoleep.Activity;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import jp.obcn.memoleep.R;
 
 /**
  * Created by iwsbrfts on 2015/05/16.
  */
 public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
 }
--- mobile/src/main/java/jp/obcn/memoleep/Activity/TopActivity.java
+++ mobile/src/main/java/jp/obcn/memoleep/Activity/TopActivity.java
@@ -1,9 +1,33 @@
 package jp.obcn.memoleep.Activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+import jp.obcn.memoleep.MainActivity;
+import jp.obcn.memoleep.R;
 
 /**
  * Created by iwsbrfts on 2015/05/16.
  */
 public class TopActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top);
+        Handler hdl = new Handler();
+        hdl.postDelayed(new splashHandler(), 2000);
+    }
+
+    class splashHandler implements Runnable {
+        public void run() {
+            // スプラッシュ完了後に実行するActivityを指定します。
+            Intent intent = new Intent(getApplication(), SettingsActivity.class);
+            startActivity(intent);
+            // SplashActivityを終了させます。
+            TopActivity.this.finish();
+        }
+    }
 }
